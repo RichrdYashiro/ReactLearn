c86bfa9a4663ae5c4717cb6485898196feb04d6f
RichrdYashiro <46631433+RichrdYashiro@users.noreply.github.com>
2025-06-02 14:09:20 +0300

    nojsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { createElement, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -8,6 +8,11 @@ function App() {
 	//императивный
 	const [count, setCount] = useState(0);
 	//декларативный
+	const currentYear = createElement(
+		"p",
+		{ className: "read-the-docs" },
+		`${new Date().getFullYear()}`
+	);
 	return (
 		<>
 			<div>
@@ -31,9 +36,8 @@ function App() {
 				<p>
 					Edit <code>src/App.jsx</code> and save to test HMR
 				</p>
+				{currentYear}
 			</div>
-			//декларативный
-			<p className="read-the-docs">{new Date().getFullYear()}</p>
 		</>
 	);
 }
